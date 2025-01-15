92105e95114c88b25b2860de0d8f2c779fd54f3c
septs <github@septs.pw>
2025-01-15 15:25:15 +0800

    improve typing

--- npm/carriers/types.ts
+++ npm/carriers/types.ts
@@ -23,21 +23,34 @@ export namespace unified {
  **/
 export namespace carrierId {
     export interface CarrierId {
+        /** A unique canonical number designated to a carrier. */
         readonly canonical_id: number;
+        /** A unique canonical number to represent its parent carrier. */
         readonly parent_canonical_id: number;
+        /** A user-friendly carrier name (not localized). */
         readonly carrier_name: string | null;
+        /** Carrier attributes to match a carrier. */
         readonly carrier_attribute: readonly CarrierAttribute[];
     }
 
     export interface CarrierAttribute {
+        /** The MCC and MNC that map to this carrier. */
         readonly mccmnc_tuple?: readonly string[];
+        /** Prefix of IMSI (International Mobile Subscriber Identity) in decimal format. */
         readonly imsi_prefix_xpattern?: readonly string[];
+        /** The Service Provider Name. Read from subscription EF_SPN. */
         readonly spn?: readonly string[];
+        /** PLMN network name. Read from subscription EF_PNN. */
         readonly plmn?: readonly string[];
+        /** Group Identifier Level 1 for a GSM phone. Read from subscription EF_GID1. */
         readonly gid1?: readonly string[];
+        /** Group Identifier Level 2 for a GSM phone. Read from subscription EF_GID2. */
         readonly gid2?: readonly string[];
+        /** The Access Point Name */
         readonly preferred_apn?: readonly string[];
+        /** Prefix of Integrated Circuit Card Identifier. Read from subscription EF_ICCID. */
         readonly iccid_prefix?: readonly string[];
+        /** Carrier Privilege Access Rule in hex string. */
         readonly privilege_access_rule?: readonly string[];
     }
 
